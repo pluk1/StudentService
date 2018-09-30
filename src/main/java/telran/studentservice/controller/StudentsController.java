d186a6f2364a1067d1c16d4362a897db79e8a03b
pluk55 <pluk@inbox.ru>
2018-09-30 12:52:39 +0300

    added Base64 auth

--- src/main/java/telran/studentservice/controller/StudentsController.java
+++ src/main/java/telran/studentservice/controller/StudentsController.java
@@ -1,6 +1,9 @@
 package telran.studentservice.controller;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
 import telran.studentservice.dto.ScoreDto;
 import telran.studentservice.dto.StudentDto;
@@ -9,6 +12,12 @@ import telran.studentservice.dto.StudentResponseDto;
 import telran.studentservice.entity.Student;
 import telran.studentservice.service.IStudentService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -20,14 +29,33 @@ public class StudentsController {
     private final static String URI = "/student";
 
 
-    @GetMapping
-    public Map<Integer,Student> getAllStudents(){
-        return studentService.getAllStudents();
+    @GetMapping(URI)
+    public Map<Integer, Student> getAllStudents(HttpServletRequest request, HttpServletResponse response) {
+
+        Map<String, String> map = new HashMap<String, String>();
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+
+        String authValue = getBasicAuth("userUser", "Password123");
+        System.out.println(authValue);
+        System.out.println(map);
+        if (map.get("authorization").equals(authValue)) {
+            return studentService.getAllStudents();
+        } else return null;
+
+
     }
 
 
     @PostMapping(URI)
     public boolean addStudent(@RequestBody StudentDto student) {
+
+
         return studentService.addStudent(student);
     }
 
@@ -49,6 +77,13 @@ public class StudentsController {
     @PutMapping("teacher/{id}")
     public boolean addScore(@PathVariable("id") int id, @RequestBody ScoreDto score) {
         return studentService.addScore(id, score);
+    }
+
+    public String getBasicAuth(String username, String password) {
+        String auth = username + ":" + password;
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+
+        return "Basic " + new String(encodedAuth);
     }
 
 }
